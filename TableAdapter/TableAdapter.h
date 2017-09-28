37a4b35fc60b2bb35243867086f8955c522be5b4
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2017-09-28 00:59:54 -0300

    Initial comm

---
+++ TableAdapter/TableAdapter.h
@@ -0,0 +1,19 @@
+//
+//  TableAdapter.h
+//  TableAdapter
+//
+//  Created by Rodrigo Kreutz on 9/28/17.
+//  Copyright © 2017 Rodrigo Kreutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableAdapter.
+FOUNDATION_EXPORT double TableAdapterVersionNumber;
+
+//! Project version string for TableAdapter.
+FOUNDATION_EXPORT const unsigned char TableAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableAdapter/PublicHeader.h>
+
+
